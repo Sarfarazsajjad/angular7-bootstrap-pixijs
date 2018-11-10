7edb2252d54547b8f59feb903b9bd7fac228981e
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2018-11-10 15:56:24 +0500

    basic setup with bootstrap and pixijs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var PIXI:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Liberti';
+export class AppComponent implements OnInit{
+  public app: any;
+
+  constructor(){
+    
+  }
+
+  ngOnInit(){
+    this.app = new PIXI.Application(window.innerWidth / 2, window.innerHeight, { backgroundColor: 0xffffff });
+    var leftPanel = document.getElementById("left-panel").appendChild(this.app.view);
+
+    var basicText = new PIXI.Text('Basic text in pixi');
+    basicText.x = 30;
+    basicText.y = 90;
+
+    this.app.stage.addChild(basicText);
+  }
+  
 }
